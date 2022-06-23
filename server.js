467e78e3e03516e779274a129f7ac81b8aa832c0
Kate Bonner <katebonner277@gmail.com>
2022-06-23 10:39:34 -0400

    setting up skeleton

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {animals} = require('./data/animals.json')
+const {notes} = require('./data/notes')
 const app = express();
 const PORT = process.env.PORT || 3001;
 
